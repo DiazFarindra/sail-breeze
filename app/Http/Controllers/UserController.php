bde47a5d46e47cf05d534d563f07bbc6b0c09078
diaz farindra <farindrad@gmail.com>
2021-10-23 11:52:52 +0700

    add groupBy test

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::get()->groupBy(function ($q) {
+            return $q->created_at->format('Y-m-d');
+        });
+        // \dump($users);
+
+        return \view('dashboard', \compact('users'));
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,31 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="p-6 mb-6 bg-white border-b border-gray-200">
                     You're logged in!
+                </div>
+                <div class="p-6 mb-6 bg-white border-b border-gray-200">
+                    <table>
+                        @foreach ($users as $day => $users_list)
+                            <tr>
+                                <th colspan="3"><strong>{{ $day }}: {{ $users_list->count() }} users<strong></th>
+                            </tr>
+                            @foreach ($users_list as $user)
+                                <tr>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>{{ $user->created_at }}</td>
+                                </tr>
+                            @endforeach
+                        @endforeach
+                    </table>
                 </div>
             </div>
         </div>
